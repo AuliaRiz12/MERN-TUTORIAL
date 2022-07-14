1f48f01110e2afa7dd53c838a33c5faf4d84e444
Aulia Rizki <191240000974@unisnu.ac.id>
2022-07-15 05:27:40 +0700

    user controller hal 24 pert 11

--- backend/controllers/usercontroller.js
+++ backend/controllers/usercontroller.js
@@ -2,9 +2,14 @@ const jwt = require ('jsonwebtoken')
 const bcrypt = require('bcryptjs')
 const asyncHandler = require('express .async.handler')
 const User = require('../models/userModel')
-const registerUser = (req, res) => {
+const registerUser = asyncHandle(async (req, res) => {
     res.json({Message: "Register User"})
-}
-
+})
+const loginUser =asyncHandler(async(req,res)=>{
+    res.json({Message: "login User"})
+})
+const getMe =asyncHandler(async(req,res)=>{
+    res.json({Message: "User data display"})
+})
 
 module.exports = { registerUser}
