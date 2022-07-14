beec077b0e6e541ce6e52805911a21d8ebf87c0d
Aulia Rizki <191240000974@unisnu.ac.id>
2022-07-14 20:40:19 +0700

    Delete Goal

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -30,9 +30,17 @@ const updateGoal = asyncHandler(async(req,res) => {
 // @desc    Delete goal
 // @route   DELETE /api/goals/:id
 // @access  Private 
-const deleteGoal = (req,res) => {
-    res.status(200).json({message: `Delete Goal ${req.params.id}`})
-}
+const deleteGoal = asyncHandler (async(req,res) => {
+    const goal = await Goal.findById(req.params.id)
+        if (!goal) {
+            res.status(400)
+            throw new Error('Goal not found')
+        }
+
+        await goal.remove()
+    
+        res.status(200).json({id: req.params.id})
+})
 module.exports = {
     getGoals,
     setGoal,
