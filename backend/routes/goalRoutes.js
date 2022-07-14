30f8726756f463c229070cf50dd3e626f3744a45
Aulia Rizki <191240000974@unisnu.ac.id>
2022-07-14 14:18:34 +0700

    connecting controller function

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,6 +1,15 @@
 const express = require('express')
 const router = express.Router()
-const { getGoals } = require('.. /controllers/goal controller')
+const {
+    getGoals,
+    setGoal,
+    updateGoal,
+    deleteGoal
+} = require ('../controllers/goalController')
+
+router.route('/').get(getGoals).post(setGoal)
+router.route('/:id').put(updateGoal).delete(deleteGoal)
+
 
 
 module.exports = router  
