42ff24d0ad36a6b2b4d2f62f5972c553be4ec9b6
Aulia Rizki <191240000974@unisnu.ac.id>
2022-07-15 05:20:23 +0700

    Menambah Package Baru

--- backend/controllers/usercontroller.js
+++ backend/controllers/usercontroller.js
@@ -1,3 +1,7 @@
+const jwt = require ('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+const asyncHandler = require('express .async.handler')
+const User = require('../models/userModel')
 const registerUser = (req, res) => {
     res.json({Message: "Register User"})
 }
