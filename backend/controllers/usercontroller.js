0334a99e91001e9d00180bda7b666ee315366aed
Aulia Rizki <191240000974@unisnu.ac.id>
2022-07-15 05:43:31 +0700

    user controllerjs hal27pert11

--- backend/controllers/usercontroller.js
+++ backend/controllers/usercontroller.js
@@ -2,7 +2,13 @@ const jwt = require ('jsonwebtoken')
 const bcrypt = require('bcryptjs')
 const asyncHandler = require('express .async.handler')
 const User = require('../models/userModel')
+
 const registerUser = asyncHandle(async (req, res) => {
+const { name, email, password } = req.body
+if (!name || !email || !password){
+    res.status(400)
+    throw Error('Please add all fields')
+}
     res.json({Message: "Register User"})
 })
 const loginUser =asyncHandler(async(req,res)=>{
