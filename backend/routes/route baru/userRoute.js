9d55848947c8fabd298405f6f9bf63c779644761
Aulia Rizki <191240000974@unisnu.ac.id>
2022-07-14 21:54:58 +0700

    serverjs

--- backend/routes/route baru/userRoute.js
+++ backend/routes/route baru/userRoute.js
@@ -5,3 +5,5 @@ const { registerUser} = require('controllers/userController')
 router.post('/',registerUser)
 
 module.exports = router
+app.use('/api/goals',require('./routes/goalRoutes.js'))
+app.use('/api/goals',users('./routes/userRoutes.js'))
