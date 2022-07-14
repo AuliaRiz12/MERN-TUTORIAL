631b4355c5d5120bce3755c057d5c6351f0a4fd4
Aulia Rizki <191240000974@unisnu.ac.id>
2022-07-14 14:04:44 +0700

    goalController

---
+++ backend/controllers/goalController.js
@@ -0,0 +1,30 @@
+//@desc    Get goals
+//@route   GET /api/goals
+//@access  Private
+const getGoals = (req,res) => {
+    res.status(200).json({messege: 'Get Goals'})
+}
+//@desc    Set goals
+//@route   POST /api/goals
+//@access  Private
+const setGoals = (req,res) => {
+    res.status(200).json({messege: 'Set Goal'})
+}
+// @desc    Update goals
+// @route   PUT /api/goals/:id
+// @access  Private 
+const updateGoal = (req,res) => {
+    res.status(200).json({message: `Update Goal ${req.params.id}`})
+}
+// @desc    Delete goal
+// @route   DELETE /api/goals/:id
+// @access  Private 
+const deleteGoal = (req,res) => {
+    res.status(200).json({message: `Delete Goal ${req.params.id}`})
+}
+module.exports = {
+    getGoals,
+    setGoal,
+    updateGoal,
+    deleteGoal
+}
