d0338514be4de6394d79ccef8f20b9b7e2e0d423
Aulia Rizki <191240000974@unisnu.ac.id>
2022-07-14 13:14:48 +0700

    file route

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,10 @@ const dotenv = require('dotenv').config()
 const port = process.env.PORT || 5000
 
 
-const app = express = require('express')
+const app = express()
 
-app.get('/api/goals',(req,re)=>{
-  res.json({message: 'Get Goals'})  
+app.get('/api/goals',(req,res)=>{
+    res.status(200).json({message: 'Get Goals'})
 })
+app.use('/api/goals',require('./routes/goalRoutes.js'))
 app.listen(port, () => console.log(`Server started on port ${port}`))
