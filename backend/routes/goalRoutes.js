2a21297b04afbb1a580cd5067d2c8dc693c20ce8
Aulia Rizki <191240000974@unisnu.ac.id>
2022-07-15 10:03:38 +0700

    Protect Goal Routes

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -4,6 +4,11 @@ const {
     registerUser,
     loginUser,
     getMe } = require('../controllers/userController.js')
+    const {protect} = require("../middleware/authMiddleware");
+
+    router.route("/").get(protect, getGoals).post(protect, setGoal);
+    router.route("/:id").put(protect, updateGoal).delete(protect, deleteGoal);
+    
 router.post('/', registerUser)
 router.post('/login', loginUser)
 router.get('/me', getMe)
